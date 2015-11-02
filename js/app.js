bea03514579c3b92544ead219ad7f52d5b0cb891
johntheo <john.theo.souza@gmail.com>
2015-11-02 17:16:52 -0200

    Commit Inicial

---
+++ js/app.js
@@ -0,0 +1,16 @@
+angular.module('app', ['firebase']);
+
+angular.module('app').controller("MainController", function($scope, $firebase) {
+  var vm = this;
+  $scope.title = 'Status Reports';
+  $scope.searchInput = '';
+  var statusResportsRef = new Firebase("https://statusreportapp.firebaseio.com/reports/");
+  $scope.statusReports = $firebase(statusResportsRef);
+
+  $scope.createReport = function() {
+    statusResportsRef.push({title: $scope.searchInput, createDate: new Date().valueOf()});
+    $scope.searchInput = '';
+  };
+
+});
+
