151cf04db1a5170c032e67f6f21d29bfa6075881
johntheo <john.theo.souza@gmail.com>
2015-11-03 12:19:18 -0200

    Navegação de telas

--- js/app.js
+++ js/app.js
@@ -1,15 +1,35 @@
 angular.module('app', ['ngAnimate', 'ui.bootstrap','firebase']);
 
 angular.module('app').controller("MainController", function($scope, $firebase) {
-  var vm = this;
-  $scope.title = 'Status Reports';
-  $scope.searchInput = '';
-  var statusResportsRef = new Firebase("https://statusreportapp.firebaseio.com/reports/");
-  $scope.statusReports = $firebase(statusResportsRef);
 
-  $scope.createReport = function() {
-    statusResportsRef.push({title: $scope.searchInput, createDate: new Date().valueOf()});
-  };
+    var vm = this;
+    $scope.title = 'Status Reports';
+    $scope.searchInput = '';
+    var statusResportsRef = new Firebase("https://statusreportapp.firebaseio.com/reports/");
+    $scope.statusReports = $firebase(statusResportsRef);
+    $scope.viewSearch = true;
+    $scope.viewReport = false;
+    $scope.viewBackButton = false;
+
+    $scope.init = function(){
+        $scope.viewSearch = true;
+        $scope.viewReport = false;
+        $scope.viewBackButton = false;
+    };
+
+    $scope.createReport = function() {
+        statusResportsRef.push({title: $scope.searchInput, createDate: new Date().valueOf()});
+    };
+
+    $scope.showReport = function(value){
+        $scope.searchInput = '';
+        $scope.report = value;
+        $scope.reportId = value.$id;
+
+        $scope.viewSearch = false;
+        $scope.viewReport = true;
+        $scope.viewBackButton = true;
+    }
 
 });
 
