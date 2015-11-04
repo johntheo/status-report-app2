ae82547f95aa9e04badf40e2a0a898ab05376eaf
johntheo <john.theo.souza@gmail.com>
2015-11-03 22:29:29 -0200

    Melhorias

--- js/app.js
+++ js/app.js
@@ -5,11 +5,15 @@ angular.module('app').controller("MainController", function($scope, $firebase) {
     var vm = this;
     $scope.title = 'Status Reports';
     $scope.searchInput = '';
+    $scope.descriptionInput = '';
+
     var statusResportsRef = new Firebase("https://statusreportapp.firebaseio.com/reports/");
     $scope.statusReports = $firebase(statusResportsRef);
+
     $scope.viewSearch = true;
     $scope.viewReport = false;
     $scope.viewBackButton = false;
+    $scope.typeInput = 'Informação';
 
     $scope.init = function(){
         $scope.viewSearch = true;
@@ -25,23 +29,32 @@ angular.module('app').controller("MainController", function($scope, $firebase) {
         $scope.searchInput = '';
         $scope.report = value;
         $scope.reportId = value.$id;
+        reportActivitiesRef = new Firebase("https://statusreportapp.firebaseio.com/reports/" + value.$id + "/activities");
+        $scope.activities = $firebase(reportActivitiesRef);
 
         $scope.viewSearch = false;
         $scope.viewReport = true;
         $scope.viewBackButton = true;
     }
 
+    $scope.createActivity = function(){
+        reportActivitiesRef.push({description: $scope.descriptionInput, type: $scope.typeInput, done: false, createDate: new Date().valueOf()});
+        $scope.descriptionInput = '';
+    }
+
 });
 
-angular.module('app').controller("ReportController", function($scope, $firebase) {
-  var statusResportsRef = new Firebase("https://statusreportapp.firebaseio.com/reports/");
-  $scope.statusReports = $firebase(statusResportsRef);
+app.filter('toArray', function () {
+    'use strict';
 
-  $scope.searchReport = function() {
-    var statusResportsRef = new Firebase("https://statusreportapp.firebaseio.com/reports/" + $scope.reportIdInput);
-    $scope.report = $firebase(statusResportsRef);
-    $scope.reportId = statusResportsRef.name();
-  };
+    return function (obj) {
+        if (!(obj instanceof Object)) {
+            return obj;
+        }
 
+        return Object.keys(obj).map(function (key) {
+            return Object.defineProperty(obj[key], '$key', {__proto__: null, value: key});
+        });
+    }
 });
 
